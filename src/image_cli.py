cb051e06c5a49f54a0f071809b0b5c0e8e16a0e9
Odai Athamneh <heyodai@gmail.com>
2022-12-10 17:41:11 -0600

    add .env file for default settings
    This commit allows (and requires) the user to create a .env file with
    default settings.

--- src/image_cli.py
+++ src/image_cli.py
@@ -1,25 +1,36 @@
 from rich import print
+from dotenv import dotenv_values
 import image_gen_class
 
 # Get a prompt from the user
 prompt = input("Enter a prompt: ")
 
+# Load in default settings from .env
+config = dotenv_values(".env")
+num_images_per_prompt = int(config["NUM_IMAGES_PER_PROMPT"])
+save_output = bool(config["SAVE_OUTPUT"])
+height = int(config["HEIGHT"])
+width = int(config["WIDTH"])
+num_inference_steps = float(config["NUM_INFERENCE_STEPS"])
+is_nsfw_forbidden = True if config["IS_NSFW_FORBIDDEN"] == "True" else False
+
 # Ask user if they wish to change the default settings
 # TODO: add a setting to automatically call MagicPrompt
-print(
+question = (
 """
 
 Default settings:
-    - num_images_per_prompt = 1
-    - Save output = True
-    - height = 512
-    - width = 512
-    - num_inference_steps = 1
-    - is_nsfw_forbidden = False
+    - num_images_per_prompt = {}
+    - Save output = {}
+    - height = {}
+    - width = {}
+    - num_inference_steps = {}
+    - is_nsfw_forbidden = {}
 
 Accept default settings? (\[y]/n)
 """
-)
+).format(num_images_per_prompt, save_output, height, width, num_inference_steps, is_nsfw_forbidden)
+print(question)
 change_settings = input()
 
 # If the user wishes to change the settings, ask them which ones
@@ -28,21 +39,12 @@ if change_settings == "n" or change_settings == "N":
     save_output = input("Enter 'y' if you wish to save the images or 'n' if you do not: ")
     height = int(input("Enter the height of the generated images: "))
     width = int(input("Enter the width of the generated images: "))
-    num_inference_steps = int(input("Enter the number of inference steps to use in the warmup pass: "))
+    num_inference_steps = float(input("Enter the number of inference steps to use in the warmup pass: "))
     is_nsfw_forbidden = input("Enter 'y' if you wish to forbid NSFW images or 'n' if you do not: ")
     if is_nsfw_forbidden == "y":
         is_nsfw_forbidden = True
     else:
         is_nsfw_forbidden = False
-
-# If the user does not wish to change the settings, use the defaults
-else:
-    num_images_per_prompt = 1
-    save_output = True
-    height = 512
-    width = 512
-    num_inference_steps = 1
-    is_nsfw_forbidden = False
 
 # Generate the images
 image_gen = image_gen_class.ImageGen(is_nsfw_forbidden)
